31dcd537100faf46cf899f1c78a0066527cee6aa
Flag-fix <isac.bandeir4@gmail.com>
2022-04-25 23:55:03 -0300

    Segundo commit layout OK

--- src/app/core/app.module.ts
+++ src/app/core/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './components/Container/Container.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { UserPictureComponent } from './components/user-picture/user-picture.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideBarComponent,
+    UserPictureComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
